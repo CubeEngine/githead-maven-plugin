b79b2ad2c3ec2350b08ddf853ea6ec279a2f3045
Phillip Schichtel <phillip.public@schich.tel>
2014-03-24 00:42:04 +0100

    removed unused imports

--- src/main/java/de/cubeisland/maven/plugins/githead/mojo/HeadMojo.java
+++ src/main/java/de/cubeisland/maven/plugins/githead/mojo/HeadMojo.java
@@ -24,14 +24,12 @@ package de.cubeisland.maven.plugins.githead.mojo;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
