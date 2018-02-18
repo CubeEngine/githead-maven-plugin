f0ce65fde3735e6212e594f79d7e04517d4e7e6c
Phillip Schichtel <phillip@schich.tel>
2018-02-18 18:45:04 +0100

    Refactor to make use of more Java 7 functionality and use the maven plugin annotations

--- src/main/java/de/cubeisland/maven/plugins/githead/mojo/HeadMojo.java
+++ src/main/java/de/cubeisland/maven/plugins/githead/mojo/HeadMojo.java
@@ -22,220 +22,149 @@
  */
 package de.cubeisland.maven.plugins.githead.mojo;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Properties;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import java.util.Properties;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.apache.maven.plugins.annotations.LifecyclePhase.INITIALIZE;
 
 /**
  * This mojo adds the git head hash as a variable
- *
- * @goal head
- * @phase initialize
- * @threadSafe
  */
+@Mojo(name = "head", defaultPhase = INITIALIZE, threadSafe = true)
 public class HeadMojo extends AbstractMojo
 {
-    /**
-     * @parameter default-value="${project}"
-     * @required
-     * @readonly
-     */
+    @Parameter(defaultValue = "${project}", readonly = true, required = true)
     private MavenProject project = null;
 
-    /**
-     * @parameter default-value="${project.basedir}"
-     */
-    protected File repoLocation = null;
+    @Parameter(defaultValue = "${project.basedir}")
+    private File repoLocation = null;
 
-    /**
-     * @parameter
-     */
-    protected boolean searchParentDirectories = true;
+    @Parameter
+    private boolean searchParentDirectories = true;
 
-    /**
-     * @parameter
-     */
-    protected String defaultBranch = "unknown";
+    @Parameter
+    private String defaultBranch = "unknown";
 
-    /**
-     * @parameter
-     */
-    protected String defaultCommit = this.defaultBranch;
+    @Parameter
+    private String defaultCommit = this.defaultBranch;
+
+    @Parameter
+    private boolean failOnFailure = false;
 
     private static final String HEAD_FILE_NAME = "HEAD";
-    private static final String HEAD_REF_PREFIX = "ref:";
+    private static final String GIT_REF_PREFIX = "ref:";
     private static final String GIT_FOLDER = ".git";
 
-    private static final Charset CHARSET = Charset.forName("UTF-8");
-
-    public void execute() throws MojoExecutionException
+    public void execute() throws MojoExecutionException, MojoFailureException
     {
-        File repo = this.repoLocation.getAbsoluteFile();
-        try
+        if (this.repoLocation == null)
         {
-            repo = repo.getCanonicalFile();
+            failOrWarn("No repo location given!", null);
+            this.repoLocation = new File(".");
         }
-        catch (IOException e)
-        {
-            getLog().debug("Failed to canonicalize the repo path!", e);
-        }
-        getLog().info("Default location: " + repo.getAbsolutePath());
-        File tmp;
-        do
-        {
-            getLog().info("Searching in: " + repo.getAbsolutePath());
-            tmp = new File(repo, GIT_FOLDER);
-            if (tmp.exists() && tmp.isDirectory())
-            {
-                getLog().info("Found at: " + repo.getAbsolutePath());
-                break;
-            }
-        }
-        while (this.searchParentDirectories && (repo = repo.getParentFile()) != null);
+        Path searchBase = this.repoLocation.toPath().toAbsolutePath();
+        getLog().info("Default location: " + searchBase.toString());
 
-        String branch = null;
-        String commit = null;
-
-        if (repo != null && repo.exists() && repo.isDirectory())
+        Path repo = searchGitRepository(searchBase, searchParentDirectories);
+        if (repo == null)
         {
-            final String head = this.readHead(repo);
-            if (head != null)
-            {
-                getLog().info("HEAD: " + head);
-                if (head.startsWith(HEAD_REF_PREFIX))
-                {
-                    final String ref = head.substring(HEAD_REF_PREFIX.length()).trim();
-                    branch = refToBranch(ref);
-                    commit = readHeadCommit(repo, ref);
-                }
-                else
-                {
-                    commit = head.trim();
-                }
-            }
+            failOrWarn("Failed to find a git repository going upwards from: " + searchBase, null);
+            setProperties(defaultBranch, defaultCommit);
         }
         else
         {
-            getLog().warn("No valid git repository found! Started from: " + this.repoLocation.getAbsolutePath());
+            try
+            {
+                final String head = readFile(repo.resolve(HEAD_FILE_NAME));
+                final String branch = headToBranch(head, defaultBranch);
+                final String commit = dereference(repo, head);
+                setProperties(branch, commit);
+            }
+            catch (IOException e)
+            {
+                failOrWarn("Failed to resolve branch and/or commit information from HEAD.", e);
+                setProperties(defaultBranch, defaultCommit);
+            }
         }
+    }
 
-        if (branch == null)
-        {
-            branch = this.defaultBranch;
-        }
-        if (commit == null)
-        {
-            commit = this.defaultCommit;
-        }
-
+    private void setProperties(String branch, String commit)
+    {
+        getLog().info("Found: " + branch + "/" + commit);
         Properties properties = this.project.getProperties();
         properties.put("githead.branch", branch);
         properties.put("githead.commit", commit);
-
-        getLog().info("Found: " + branch + "/" + commit);
     }
 
-    private static String refToBranch(String ref)
+    private void failOrWarn(String message, Throwable cause) throws MojoFailureException
     {
-        String[] parts = ref.split("/");
-        String branch = parts[parts.length - 1].trim();
-        if ("".equals(branch))
+        if (failOnFailure)
+        {
+            throw new MojoFailureException(message, cause);
+        }
+        else
+        {
+            getLog().warn(message);
+        }
+    }
+
+    private static Path searchGitRepository(Path current, boolean checkParents)
+    {
+        if (current == null)
         {
             return null;
         }
-        return branch;
-    }
-
-    private String readHeadCommit(File repo, String ref)
-    {
-        File refFile = new File(repo, GIT_FOLDER + File.separator + ref);
-
-        BufferedReader reader = null;
-        String commitHash = null;
-        try
+        Path gitDir = current.resolve(GIT_FOLDER);
+        if (Files.isDirectory(gitDir))
         {
-            reader = new BufferedReader(new InputStreamReader(new FileInputStream(refFile), CHARSET));
-
-            while ((commitHash = reader.readLine()) != null)
-            {
-                commitHash = commitHash.trim();
-                if (!"".equals(commitHash))
-                {
-                    break;
-                }
-            }
+            return gitDir;
         }
-        catch (FileNotFoundException e)
+        if (checkParents)
         {
-            getLog().warn("The file " + refFile.getPath() + " could not be found!", e);
-        }
-        catch (IOException e)
-        {
-            getLog().warn("Failed to read the ref file", e);
-        }
-        finally
-        {
-            if (reader != null)
-            {
-                try
-                {
-                    reader.close();
-                }
-                catch (IOException e)
-                {
-                    getLog().debug("Failed to close the ref file!", e);
-                }
-            }
-        }
-        return commitHash;
-    }
-
-    private String readHead(File repo)
-    {
-        File headFile = new File(repo, GIT_FOLDER + File.separator + HEAD_FILE_NAME);
-
-        BufferedReader reader = null;
-        try
-        {
-            reader = new BufferedReader(new InputStreamReader(new FileInputStream(headFile), CHARSET));
-
-            String line = reader.readLine();
-            if (line != null)
-            {
-                return line.trim();
-            }
-        }
-        catch (FileNotFoundException e)
-        {
-            getLog().warn("The file " + headFile.getPath() + " could not be found!", e);
-        }
-        catch (IOException e)
-        {
-            getLog().warn("Failed to read the " + HEAD_FILE_NAME + " file", e);
-        }
-        finally
-        {
-            if (reader != null)
-            {
-                try
-                {
-                    reader.close();
-                }
-                catch (IOException e)
-                {
-                    getLog().debug("Failed to close the HEAD file!", e);
-                }
-            }
+            return searchGitRepository(current.getParent(), true);
         }
         return null;
+    }
+
+    private static String headToBranch(String headRef, String def)
+    {
+        if (headRef.startsWith(GIT_REF_PREFIX))
+        {
+            String target = headRef.substring(GIT_REF_PREFIX.length()).trim();
+            String[] parts = target.split("/");
+            if (parts.length > 1)
+            {
+                return parts[parts.length - 1];
+            }
+        }
+        return def;
+    }
+
+    private String dereference(Path repo, String ref) throws IOException
+    {
+        if (ref.startsWith(GIT_REF_PREFIX))
+        {
+            String target = ref.substring(GIT_REF_PREFIX.length()).trim();
+            return dereference(repo, readFile(repo.resolve(target)));
+        }
+        else
+        {
+            return ref;
+        }
+    }
+
+    private static String readFile(Path path) throws IOException
+    {
+        return new String(Files.readAllBytes(path), UTF_8).trim();
     }
 }
